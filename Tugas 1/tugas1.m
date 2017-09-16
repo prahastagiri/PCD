5d5b7ecc59281428fcdf41ccfcf12cc95a88cd0e
prahastagiri <iputuadiprahastagiri@gmail.com>
2017-09-16 19:01:23 +0700

    First Upload

---
+++ Tugas 1/tugas1.m
@@ -0,0 +1,91 @@
+function varargout = tugas1(varargin)
+% TUGAS1 MATLAB code for tugas1.fig
+%      TUGAS1, by itself, creates a new TUGAS1 or raises the existing
+%      singleton*.
+%
+%      H = TUGAS1 returns the handle to a new TUGAS1 or the handle to
+%      the existing singleton*.
+%
+%      TUGAS1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in TUGAS1.M with the given input arguments.
+%
+%      TUGAS1('Property','Value',...) creates a new TUGAS1 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before tugas1_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to tugas1_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help tugas1
+
+% Last Modified by GUIDE v2.5 04-Sep-2017 17:33:02
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @tugas1_OpeningFcn, ...
+                   'gui_OutputFcn',  @tugas1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before tugas1 is made visible.
+function tugas1_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to tugas1 (see VARARGIN)
+
+% Choose default command line output for tugas1
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes tugas1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = tugas1_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global im im2
+[path, user_cance]=imgetfile();
+if user_cance
+    msgbox(sprintf('Error'),'Error','Error');
+    return
+end
+im=imread(path);
+im=im2double(im);
+im2=im;
+axes(handles.axes3);
+imshow(im);
